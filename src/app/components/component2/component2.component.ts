04027b5f8b7fe6a8df4af976c164c275a25dcc35
Valeria <yokoyamamoto92@gmail.com>
2023-01-02 18:37:52 -0500

    mitt

--- src/app/components/component2/component2.component.ts
+++ src/app/components/component2/component2.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-component2',
   templateUrl: './component2.component.html',
   styleUrls: ['./component2.component.css']
 })
-export class Component2Component {
+export class Component2Component implements OnInit {
 
+  Usuario:any
+  Gmail:any
+  Password:any
+  Confirm:any
+  Cell:any
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.recuperar()
+  }
+
+  recuperar(){
+    this.Usuario = localStorage.getItem('Usuario')
+    this.Gmail = localStorage.getItem('Gmail')
+    this.Password = localStorage.getItem('Password')
+  }
 }
