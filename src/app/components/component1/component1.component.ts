2585a80659b0d747d00c99ee073e1e515f6f4848
Valeria <yokoyamamoto92@gmail.com>
2023-01-02 15:54:29 -0500

    mas

--- src/app/components/component1/component1.component.ts
+++ src/app/components/component1/component1.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-component1',
   templateUrl: './component1.component.html',
   styleUrls: ['./component1.component.css']
 })
-export class Component1Component {
+export class Component1Component implements OnInit{
+  num1:number;
+  num2:number;
+  num3:number;
+  num4:number;
+  num5:number;
 
+  constructor(private router:Route){}
+
+  ngOnInit(): void {
+    localStorage.clear()
+  }
+  
+  almacenar(){
+    localStorage.setItem('Usuario', this.num1.toString())
+    localStorage.setItem('Gmail', this.num2.toString())
+    localStorage.setItem('Password', this.num3.toString())
+    localStorage.setItem('Confirm', this.num4.toString())
+    localStorage.setItem('Cell', this.num5.toString())
+  }
 }
