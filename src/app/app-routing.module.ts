2fb9a9a9486f0de1e0418a078c3be534b114fcc1
unknown <you@example.com>
2023-01-05 12:02:31 -0500

    hjfoil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes,RouterModule  } from '@angular/router';
 import { Component1Component } from './components/component1/component1.component';
 import { Component2Component } from './components/component2/component2.component';
 
